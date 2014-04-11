3912b929c70bb5255a82bf5c19cddff79f5bf5b3
Taras Slipets <taras.slipets@gmail.com>
2014-04-11 23:23:47 +0100

    renamed classes to reflect proper pattern model

--- src/ua/kpi/fpm/pma/oop/lecture7/mvc/model/AuthenticationModel.java
+++ src/ua/kpi/fpm/pma/oop/lecture7/mvc/model/AuthenticationModel.java
@@ -1,8 +1,5 @@
 package ua.kpi.fpm.pma.oop.lecture7.mvc.model;
 
-/**
- * Created by Taras on 10.04.2014.
- */
 public interface AuthenticationModel {
     boolean authenticate(String login, String password);
 }
